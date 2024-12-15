9bf3a0e5150ad7a7f6a8549f17244d1331161488
chuckynuttys <charles25strain@gmail.com>
2024-12-15 17:24:36 -0600

    HookApi
    Axios and using catfact API to fetch query

--- hookapi/src/App.js
+++ hookapi/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Cat } from "./Components/Cat";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <div className="App"></div>;
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
+  return (
+    <div className="App">
+      <QueryClientProvider client={client}>
+        <Cat />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ hookapi/src/useGetCat.js
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
+export const useGetCat = () => {
+  const {
+    data,
+    refetch,
+    isLoading: isCatLoading,
+    error,
+  } = useQuery({
+    queryKey: ["cat"],
+    queryFn: async () => {
+      const res = await Axios.get("https://catfact.ninja/fact");
+      return res.data;
+    },
+  });
+
+  const refetchData = () => {
+    alert("DATA REFETCHED");
+    refetch();
+  };
+
+  return { data, refetchData, isCatLoading };
+};
